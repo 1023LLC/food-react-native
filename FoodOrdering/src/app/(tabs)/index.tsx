e44fe8a79fb13287a759c5511c35c55857660f07
julius <nyerereoffice@gmail.com>
2024-06-09 19:39:54 +0300

    Implementing TypeScript Path aliases

--- FoodOrdering/src/app/(tabs)/index.tsx
+++ FoodOrdering/src/app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
-import { View } from '@/src/components/Themed';
+import { View } from '@components/Themed';
 import products from '../../../assets/data/products';
-import ProductListItem from '@/src/components/ProductListItem';
+import ProductListItem from '@components/ProductListItem';
 
 export default function MenuScreen() {
   return (
