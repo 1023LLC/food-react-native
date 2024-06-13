edd4f2495c79e527cdb58beb3f8215322798bdda
julius <nyerereoffice@gmail.com>
2024-06-13 04:34:03 +0300

    FlatList Implementation

--- FoodOrdering/src/app/(tabs)/index.tsx
+++ FoodOrdering/src/app/(tabs)/index.tsx
@@ -1,12 +1,17 @@
-import { View } from '@components/Themed';
 import products from '../../../assets/data/products';
 import ProductListItem from '@components/ProductListItem';
+import { FlatList, View,  } from 'react-native';
 
 export default function MenuScreen() {
   return (
     <View>
-      <ProductListItem product={products[5]}/>
-      <ProductListItem product={products[1]}/>
+      <FlatList 
+        data={products}
+        renderItem={({ item }) => <ProductListItem product={item} />}
+        numColumns={2}
+        contentContainerStyle={{ gap: 10, padding: 10 }}
+        columnWrapperStyle={{ gap:10 }}
+      />
     </View>
    
   );
